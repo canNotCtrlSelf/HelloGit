ca3c3d4aeaf67f3f8859c92c7187452202c791c8
zhangsan <zhangsan@cncs.com>
2020-09-23 13:09:51 +0800

    add String2Date()

--- src/main/java/com/cncs/utils/DateUtils.java
+++ src/main/java/com/cncs/utils/DateUtils.java
@@ -4,9 +4,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtils {
-    public  String Date2String(Date date,String patt){
+    public String Date2String(Date date, String patt) {
         SimpleDateFormat sdf = new SimpleDateFormat(patt);
         String format = sdf.format(date);
         return format;
+    }
+
+    public Date String2Date(String date,String patt){
+        return new Date();
     }
 }
